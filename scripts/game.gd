a7ff47f862645840e9684aa1885096199e6bcb87
Nocturnal <214055938+Nocturnal-sys@users.noreply.github.com>
2025-12-14 03:22:18 +0000

    Add timer on screen

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,8 +4,14 @@ extends Node2D
 @onready var cup: AnimatableBody2D = $Cup
 @onready var ball: RigidBody2D = $Ball
 @onready var rope: Node2D = $Rope
+@onready var time_left: Label = $TimeLeft
+@onready var timer: Timer = $Timer
 
 
 func _ready() -> void:
 	rope.joints[0].node_a = cup.get_path()
 	rope.joints[-1].node_b = ball.get_path()
+
+
+func _process(delta: float) -> void:
+	time_left.text = str(int(timer.time_left))
